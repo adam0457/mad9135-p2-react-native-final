031bd08cd0c52c7d5a503417cd8e5ed422614a73
adam0457 <adam0457@algonquinlive.com>
2022-12-14 08:12:50 -0500

    Add the favorite icon so the user can like a book

--- components/BookDetails.js
+++ components/BookDetails.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {useState} from 'react'
+import {View, Text, Image, StyleSheet, Pressable} from 'react-native'
 import { useBook } from '../context/BookContext'
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 export default function BookDetails({route}){
   const [books] = useBook()
   const selectedCategory = books.find(item => item.categoryId === route.params.categoryId)
   const selectedBook = selectedCategory.items.find(item => item.bookId === route.params.bookId)
+
+  const [like, setLike] = useState(true)
+
+  function changeIconName(){
+    setLike(!like)
+  }
   
-
-
   return(
 
-    <Text>
-      {selectedBook.bookName}
-    </Text>
+    <View>
+      <Image style = {styles.img} source={require(`../assets/img/${selectedBook.image}.jpg`)}/>
+      <Text>{selectedBook.bookName}</Text>
+      <Pressable onPress={changeIconName} >
+          <MaterialIcons name={like ? "favorite-outline" : "favorite" } size={32} color="red" />
+      </Pressable>  
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  
+
+  },
+  img:{
+    width: 250,
+    height:250
+  }
+})
